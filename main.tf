0b7763e99c5235e49c15f5607cae5b7a53eaaf38
SaiPriya232 <110330467+SaiPriya232@users.noreply.github.com>
2022-11-29 12:46:39 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "example" {
+  name     = "priya"
+  location = "West Europe"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "priyaregistry"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "Premium"
+  admin_enabled       = false
+  georeplications {
+    location                = "East US"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+  georeplications {
+    location                = "North Europe"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+}
