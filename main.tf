8328d84ff44b41cf0d4bdf4eb2f35422582c74fc
SaiPriya232 <110330467+SaiPriya232@users.noreply.github.com>
2022-11-29 12:52:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,12 +12,12 @@ provider "azurerm" {
   features {}
 }
 resource "azurerm_resource_group" "example" {
-  name     = "priya"
+  name     = "saipriya"
   location = "West Europe"
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "priyaregistry"
+  name                = "saipriyaregistry"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   sku                 = "Premium"
